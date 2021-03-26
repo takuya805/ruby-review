1cbb60acfa1585ab78009cbe93be4550a3556ce8
takuya805 <takuya0903exile@gmail.com>
2021-03-26 23:48:16 +0900

    ruby-practice

--- ruby20.rb
+++ ruby20.rb
@@ -1,20 +1,26 @@
 class Box
-  def initalize(item)
-    @item = item
-  end
 
+  def initialize(item)
+     @item=item
+  end
+  
   def open()
-    puts "宝箱を開いた。#{@item}を手に入れた。"
+     puts "宝箱を開いた。#{@item}を手に入れた。"
   end
-end
-
-class JewelryBox < Box
-  def look()
-    puts "宝箱はキラキラ輝いている。"
-  end
-end
-
-jewelrybox = JewelryBox.new("魔法の指輪")
-ewelrybox.look()
-jewelrybox.open()
+ end
+ 
+ class JewelryBox < Box
+     def look()
+         puts "宝箱はキラキラ輝いている。"
+     end
+ end
+ 
+ box=Box.new("鋼鉄の剣")
+ box.open()
+ 
+ puts
+ 
+ jewelrybox=JewelryBox.new("魔法の指輪")
+ jewelrybox.look()
+ jewelrybox.open()
 
